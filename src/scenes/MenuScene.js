64b208a463543180102529c5a1536ab7d815f2af
Fyrthir <seb.vaesen@gmail.com>
2020-09-15 19:50:28 +0100

    Update MenuScene.js

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -20,7 +20,7 @@ export class MenuScene extends Phaser.Scene{
         
         console.log('Music_Game');
         this.game.music = this.game.sound.add('Music_Game');
-
+        this.game.music.muted = true;
         this.game.music.play();
 
     }
